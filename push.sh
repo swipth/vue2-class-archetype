7c5d2be5f7c7c841c9c95fb1a2aaf601b0009f87
swipth <13218037380@163.com>
2023-06-14 15:21:33 +0800

    [swipth] #V0.1.0 fix:  refactor:  feat:  style: chore: 仓库备份

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 git add .
-git commit -m "[swipth] #V0.1.0 fix:  refactor:  feat:  style: chore: 修改配置"
+git commit -m "[swipth] #V0.1.0 fix:  refactor:  feat:  style: chore: 仓库备份"
 git pull origin --rebase
 git push origin
+git push gitee
