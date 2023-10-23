3941a91a8135d8593bb74ba7a4bd5fd7ed50553d
swipth <13218037380@163.com>
2023-10-23 10:53:27 +0800

    feat: init

---
+++ src/mixins/Query.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Author: swipth
+ * @Description:
+-->
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Query extends Vue {
+  mounted() {
+    this.resize();
+    this.setHeight();
+  }
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.setHeight);
+  }
+  contentHeight=240
+  tableHeight = 700;
+
+  resize() {
+    window.addEventListener("resize", () => {
+      requestAnimationFrame(this.setHeight);
+    });
+  }
+
+  setHeight() {
+    this.tableHeight = window.innerHeight - this.contentHeight;
+  }
+
+}
+</script>
+
+<template>
+  <div>Query</div>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
