26fec7dbade3455e4e1ebee61eaa58fe3aa74c0b
swipth <13218037380@163.com>
2023-12-26 16:02:28 +0800

    feat: init

--- src/api/config/network.ts
+++ src/api/config/network.ts
@@ -2,22 +2,7 @@
  * @description 导出网络配置
  **/
 
-export const networkKey: {
-  contentType: string;
-  contentJsonType: string;
-  contentTextType: string;
-  contentMultipartFormType: string;
-  requestTimeout: number;
-  messageName: string;
-  statusName: string;
-  successName: string;
-  successCode: (string | number)[];
-  dataName: string;
-  noShowApiMessage: string[];
-  loginPath: string;
-  messageDuration:number;
-  defaultLogoutTime:number
-} = {
+export const networkKey = {
   // 网络传输类型 // form-data 一般配合qs
   contentType: "application/x-www-form-urlencoded;charset=UTF-8",
   // json
@@ -40,7 +25,7 @@ export const networkKey: {
   // 数据字段名称
   dataName: "data",
   // 后台哪些api接口不走消息提示
-  noShowApiMessage: [],
+  noShowApiMessage: [""],
   // 登录地址
   loginPath:"/auth/login",
   // 消息框消失时间
